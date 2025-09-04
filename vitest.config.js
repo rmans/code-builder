4afb4d326d9376c4146e95b3c49ff4ca71bf0722
rmans <rmans87@yahoo.com>
2025-09-04 15:17:20 -0400

    chore: ignore artifacts, untrack compiled files, add requirements and CI

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-export default defineConfig({
-    test: {
-        include: ['test/**/*.test.ts'],
-        environment: 'node',
-        globals: true,
-    },
-});
-//# sourceMappingURL=vitest.config.js.map
