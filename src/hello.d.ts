57ea2293f34e9fa9ecbede140763b5a678033f17
rmans <rmans87@yahoo.com>
2025-09-05 20:59:44 -0400

    cleanup: remove src/ directory and compiled artifacts from master
    - Remove src/ directory containing compiled TypeScript files
    - Remove .js, .d.ts, .map files that should not be tracked
    - Clean up master branch before merging feat/doc-factory

--- src/hello.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns a greeting.
- * @param name Optional name to greet.
- * @returns Greeting string.
- */
-export declare const hello: (name?: string) => string;
-//# sourceMappingURL=hello.d.ts.map
